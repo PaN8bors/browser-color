08c05a42f99d5c21f74522066f660f1e2ce6bc5d
Parish Neighbors <pn8bors@gmail.com>
2022-02-24 10:52:37 -0600

    First commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 function main() {
+  const myBox = document.getElementById("my-box");
+  myBox.addEventListener('click', () => {
+    if (myBox.style.backgroundColor === "red") {
+      myBox.style.backgroundColor = "black";
+    } else {
+      myBox.style.backgroundColor = "red"
+    }
+  });
+
+
+
+
+
   // Get the "my-box" div from the document
   // When someone clicks on the element then change the background color
   // to red.
